d97497cd30f644da43ec079f4d5af509d5f09b1c
Chris Farley <cfarley95@gmail.com>
2019-05-22 19:37:31 -0400

    more test

--- app.py
+++ app.py
@@ -228,8 +228,8 @@ def newEvent():
             cursor.execute(sql, (data["name"], data["description"], data["company"], data["userid"], data["eventid"],data["datenum"],data["endtime"],
                                  data["latitude"],data["longitude"],data["address"],
                                  data["photos"],
-                                 Point(data["longitude"],
-                                       data["latitude"])))
+                                 Point(longitude,
+                                       data[latitude])))
             print(cursor)
             connection.commit()
     finally:
