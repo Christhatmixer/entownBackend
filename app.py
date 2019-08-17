4e5af5b74bf9ea95e0368bdd008124bd0b2357fb
Chris Farley <cfarley95@gmail.com>
2019-08-17 03:30:36 -0400

    fix

--- app.py
+++ app.py
@@ -357,7 +357,7 @@ def newEvent():
             print(latitude)
             location = Point(latitude, longitude)
             print(location.x)
-            psycopg2.extensions.cursor.execute()
+            
             sql = "INSERT INTO events (name,description,company,userid,eventid,starttimestamp,endtimestamp,endtime,latitude,longitude,address,location) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
             cursor.execute(sql, (data["name"], data["description"], data["company"], data["userid"], data["eventid"],
                                  data["starttimestamp"], data["endtimestamp"], data["endtime"], data["starttime"],
