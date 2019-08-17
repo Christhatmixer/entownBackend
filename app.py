f2620e078b758f5c5fa129ccf5d9d3c30a575d81
Chris Farley <cfarley95@gmail.com>
2019-08-17 00:49:54 -0400

    sql fix

--- app.py
+++ app.py
@@ -351,8 +351,8 @@ def newEvent():
             cursor.execute(sql, (data["name"], data["description"], data["company"], data["userid"], data["eventid"],
                                  data["starttimestamp"],data["endtimestamp"],data["endtime"],data["starttime"],
                                  data["latitude"],data["longitude"],data["address"],
-                                 Point(data["longitude"],
-                                       data["latitude"])))
+                                 Point(longitude,
+                                       latitude)))
             print(cursor)
             connection.commit()
     finally:
