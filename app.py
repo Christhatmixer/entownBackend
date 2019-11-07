c278a5f9722cf2e6c2bb464e4410caeb8fab127b
Chris Farley <cfarley95@gmail.com>
2019-11-07 05:22:24 -0500

    OneSignal fix

--- app.py
+++ app.py
@@ -568,8 +568,8 @@ def likePost():
 
         payload = {"app_id": "e866c9f9-c862-4a1e-b605-b1a0820ca658",
                    "include_external_user_ids": ["%s" % data["otheruserid"]],
-                   "contents": {"en": "%s liked your post." % data["username"]},
-                   "headings": {"en": "English Title"}}
+                   "contents": {"en": "%s liked your post." % data["username"]}
+                   }
 
         req = requests.post("https://onesignal.com/api/v1/notifications", headers=header, data=json.dumps(payload))
 
