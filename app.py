5d50a99907d919f9b9c6cae4057d59e5feb647e4
Chris Farley <cfarley95@gmail.com>
2019-11-07 03:00:39 -0500

    like notifications

--- app.py
+++ app.py
@@ -567,6 +567,15 @@ def likePost():
                 pushy.push(data["otherdevicetoken"], data)
 
     finally:
+        header = {"Content-Type": "application/json; charset=utf-8"}
+
+        payload = {"app_id": "e866c9f9-c862-4a1e-b605-b1a0820ca658",
+                   "include_player_ids": [data["otheruserid"]],
+                   "contents": {"en": data["username"] + " "+ "liked your post."}}
+
+        req = requests.post("https://onesignal.com/api/v1/notifications", headers=header, data=json.dumps(payload))
+
+        print(req.status_code, req.reason)
 
 
         connection.close()
