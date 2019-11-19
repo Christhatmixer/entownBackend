4a02bb009f4dee9b7af1c269c330cac2ab051845
Chris Farley <cfarley95@gmail.com>
2019-11-18 19:57:54 -0500

    sql fix

--- app.py
+++ app.py
@@ -1018,7 +1018,7 @@ def getSubscribers():
     dict_cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     try:
         with dict_cur as cursor:
-            sql = '''SELECT * FROM users,exists(select 1 from followings where followings.userid = %s and followings.followingid = users.userid limit 1) as isFollowed 
+            sql = '''SELECT users.*,exists(select 1 from followings where followings.userid = %s and followings.followingid = users.userid limit 1) as isFollowed FROM users
             INNER JOIN  followings ON users.userid = followings.followingid 
             WHERE followings.followingid = %s
             '''
