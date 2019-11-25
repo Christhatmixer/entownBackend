9dcdd2615fa2d8805e84d40b5773742719658a65
Chris Farley <cfarley95@gmail.com>
2019-11-25 07:01:09 -0500

    sql fix

--- app.py
+++ app.py
@@ -133,6 +133,8 @@ def getPostFeed():
         with dict_cur as cursor:
             sql = '''SELECT post.*,exists(select 1 from likes  where likes.postId = post.postid and likes.userid = %s limit 1) as liked,COUNT(likes.postid) AS like_count,COUNT(comments.postid) AS comment_count
              FROM post 
+            LEFT JOIN likes ON post.postid = likes.postid
+            LEFT JOIN "comments" ON post.postid = "comments".postid
             INNER JOIN followings ON post.userid = followings.followingid 
             INNER JOIN users on post.userid = users.userid 
             WHERE followings.userid = %s
