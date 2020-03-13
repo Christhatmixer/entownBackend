213c61c21c2c0be3392f8691bcafca91e9667cbe
Chris Farley <cfarley95@gmail.com>
2020-03-13 13:14:24 -0400

    sql fix

--- app.py
+++ app.py
@@ -528,7 +528,7 @@ def getUserInfo():
         with dict_cur as cursor:
             sql = '''
             SELECT users.*,COUNT(followings.followingid) AS follower_count,COUNT(followings.userid) as following_count FROM users 
-            inner join followings on users.userid = followings.followingid
+            left join followings on users.userid = followings.followingid
             WHERE users.userid = %s group by users.bio,users.companyname,
             users.devicetoken,users.email,users."name",users.profileimageurl,users.radius,users.userid,users.username
             '''
